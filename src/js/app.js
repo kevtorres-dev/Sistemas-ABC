99d3eefbefaecbe6fec77f5415852a81b528fdbd
Kevin Torres <kevintorresdev@gmail.com>
2024-10-02 15:35:38 -0600

    Redesign / Tercera Prueba / Videos Locales

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,23 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+const menuLinks = document.querySelectorAll('.navegacion a[href^="#"')
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", function(){
+        menu.classList.remove("menu_opened");
+    })
+})
+
+document.querySelectorAll('.videos-contenedor video').forEach(vid => {
+    vid.onclick = () => {
+        document.querySelector('.popup-video').style.display = 'block';
+        document.querySelector('.popup-video video').src = vid.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-video span').onclick = () => {
+    document.querySelector('.popup-video').style.display = 'none';
+    document.querySelector('.popup-video video').pause();
+}
