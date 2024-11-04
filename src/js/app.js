aa982992e6d414a3ef2710ad0c9c3a2920b01c71
Kevin Torres <kevintorresdev@gmail.com>
2024-11-04 17:48:12 -0600

    Sections: Intro/Info

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,25 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+
+
+
+
+//VIDEO MODAL
+
+cover_video = document.getElementById("contenedor-video-cover");
+video = document.getElementById("video");
+
+function mostrar_video() {
+    cover_video.style.display = "flex";
+    video.src = "https://www.youtube.com/embed/dEYHCHgxTgI?si=C7iryBPklVTR5Y-y";
+}
+
+function ocultar_video() {
+    cover_video.style.display = "none";
+    video.src = "";
+}
+
+document.getElementById("btn-video").addEventListener("click", mostrar_video);
+document.getElementById("cerrar-video").addEventListener("click", ocultar_video);
